ab79dd96d99ea24afc861bca82569e72ce30a7fc
Sraddheya <ggsraddheya@gmail.com>
2021-04-23 16:43:54 +0100

    CateringCompanyClientImp finished

--- src/main/java/shield/CateringCompanyClientImp.java
+++ src/main/java/shield/CateringCompanyClientImp.java
@@ -8,9 +8,14 @@ public class CateringCompanyClientImp implements CateringCompanyClient {
   private String name;
   private String postcode;
 
-  public class IncorrectFormatException extends Exception {
+  public class CustomException extends Exception {
 
-    public IncorrectFormatException(String message) {
+    /**
+     * Custom exception to help identify the exact cause through the error message.
+     *
+     * @param message the message thrown that informs the user of the exact issue
+     */
+    public CustomException(String message) {
       super(message);
     }
   }
@@ -18,13 +23,19 @@ public class CateringCompanyClientImp implements CateringCompanyClient {
   public CateringCompanyClientImp(String endpoint) { this.endpoint = endpoint; }
 
   /**
-   * Returns true if the operation occurred correctly (catering company is
-   * registered or already registered).
+   * Returns true if the operation occurred correctly (catering company is registered
+   * or already registered).
+   *
+   * We decided to throw an exception if the postcode does not start with EH and is not
+   * separated by an underscore, e.g, EH11_2DR, so that the post code is compatible with
+   * the rest of the system (to be precise the method getDistance in the
+   * ShieldingIndividualClientImp class).
    *
    * @param name name of the business
    * @param postCode post code of the business
    * @return true if the operation occurred correctly
-   * @Exception if the format of the postcode is incorrect
+   * @CustomException if the format of the postcode is incorrect
+   * @Exception if http request unsuccessful
    */
   @Override
   public boolean registerCateringCompany(String name, String postCode) {
@@ -32,20 +43,20 @@ public class CateringCompanyClientImp implements CateringCompanyClient {
     assert(!name.equals(null) && !postCode.equals(null));
 
     // Make sure postCode format is correct
-    if (!postCode.startsWith("EH") && !postCode.contains("_")) {
+    if (!postCode.startsWith("EH") || !postCode.contains("_")) {
       try {
-        throw new IncorrectFormatException("Postcodes must start with EH and be separated by an underscore");
-      } catch (IncorrectFormatException e) {
+        throw new CustomException("Postcodes must start with EH and be separated by an underscore");
+      } catch (CustomException e) {
         e.printStackTrace();
       }
       return false;
     }
 
-    // construct the endpoint request
+    // Construct the endpoint request
     String request = "/registerCateringCompany?business_name=" + name + "&postcode=" + postCode;
 
     try {
-      // perform request
+      // Perform request
       String response = ClientIO.doGETRequest(endpoint + request);
 
       if (response.equals("registered new") || response.equals("already registered")) {
@@ -66,17 +77,18 @@ public class CateringCompanyClientImp implements CateringCompanyClient {
    * @param orderNumber the order number
    * @param status status of the order for the requested number
    * @return true if the operation occurred correctly
+   * @Exception if http request unsuccessful
    */
   @Override
   public boolean updateOrderStatus(int orderNumber, String status) {
     // Make sure parameters are not null
     assert(orderNumber>=0 && !status.equals(null));
 
-    // construct the endpoint request
+    // Construct the endpoint request
     String request = "/updateOrderStatus?order_id=" + orderNumber + "&newStatus=" + status;
 
     try {
-      // perform request
+      // Perform request
       String response = ClientIO.doGETRequest(endpoint + request);
 
       if (response.equals("True")) {
--- src/test/java/shield/CateringCompanyClientImpTest.java
+++ src/test/java/shield/CateringCompanyClientImpTest.java
@@ -1,8 +1,6 @@
 
 package shield;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -10,6 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import java.io.IOException;
 import java.lang.reflect.Type;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 import java.time.LocalDateTime;
 import java.io.InputStream;
@@ -42,23 +41,27 @@ public class CateringCompanyClientImpTest {
   }
 
   /**
-   * Systems tests
+   * We did not write separate unit and systems tests for CateringCompanyClientImpTest as we felt that
+   * each method encompassed a use case in and of itself.
    */
   @Test
   public void testCateringCompanyNewRegistration() {
+    // Generate post code
     Random rand = new Random();
     String name = String.valueOf(rand.nextInt(10000));
-    String postCode_part1 = String.valueOf(rand.nextInt(10));
-    String postCode_part2 = String.valueOf(rand.nextInt(1000));
-    String postCode = "EH" + postCode_part1 + "_" + postCode_part2;
+    String temp1 = String.valueOf(rand.nextInt(10));
+    String temp2 = Character.toString( (char) (rand.nextInt(26) + 65));
+    String temp3 = Character.toString( (char) (rand.nextInt(26) + 65));
+    String postCode = "EH" + temp1 + "_" + temp1 + temp2 + temp3;
 
     // Null parameters should be asserted
-    //assertTrue(client.registerCateringCompany(null, null));
-    //assertTrue(client.registerCateringCompany(name, null));
-    //assertTrue(client.registerCateringCompany(null, postCode));
+    //client.registerCateringCompany(null, null);
+    //client.registerCateringCompany(name, null);
+    //client.registerCateringCompany(null, postCode);
 
     // Incorrect postCode format should give exception
-    assertFalse(client.registerCateringCompany(name, postCode_part1));
+    assertFalse(client.registerCateringCompany(name, "EH" + temp1));
+    assertFalse(client.registerCateringCompany(name, temp1 + "_"));
 
     // Correct new registration
     assertTrue(client.registerCateringCompany(name, postCode));
@@ -75,14 +78,13 @@ public class CateringCompanyClientImpTest {
 
   @Test
   public void testUpdateOrderStatus() {
+    // Registering new Shielding Individual to place orders
     Random rand = new Random();
-    String invalidStatus = String.valueOf(rand.nextInt(10000));
-    int falseOrderNumber = rand.nextInt(10000);
-    int orderNumber = 0;
-    String[] statuses = {"packed", "dispatched", "delivered"};
-
-    // Registering new Shielding Individual to make orders
-    String CHI = String.valueOf(rand.nextInt(10000));
+    String temp = String.valueOf(rand.nextInt(10000 - 1000) + 1000);
+    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("ddMMyy");
+    LocalDateTime now = LocalDateTime.now();
+    String date = dtf.format(now);
+    String CHI = date + temp;
     String request1 = "/registerShieldingIndividual?CHI=" + CHI;
     try {
       ClientIO.doGETRequest(clientProps.getProperty("endpoint") + request1);
@@ -90,9 +92,12 @@ public class CateringCompanyClientImpTest {
       ex.printStackTrace();
     }
 
-    // Registering new Shielding Individual to make orders
+    // Registering new Catering Company to update Orders
     String name = String.valueOf(rand.nextInt(10000));
-    String postCode = String.valueOf(rand.nextInt(10000));
+    String temp1 = String.valueOf(rand.nextInt(10));
+    String temp2 = Character.toString( (char) (rand.nextInt(26) + 65));
+    String temp3 = Character.toString( (char) (rand.nextInt(26) + 65));
+    String postCode = "EH" + temp1 + "_" + temp1 + temp2 + temp3;
     String request2 = "/registerCateringCompany?business_name=" + name + "&postcode=" + postCode;
     try {
       ClientIO.doGETRequest(clientProps.getProperty("endpoint") + request2);
@@ -100,9 +105,10 @@ public class CateringCompanyClientImpTest {
       ex.printStackTrace();
     }
 
-    // Placing order
+    // Place order
+    int orderNumber = 0;
     String request3 = "/placeOrder?individual_id=" + CHI + "&catering_business_name=" + name + "&catering_postcode=" + postCode;
-    String data = "{\"contents\": [{\"id\":1,\"name\":\"cucumbers\",\"quantity\":20},{\"id\":2,\"name\":\"tomatoes\",\"quantity\":2}]}";
+    String data = "{\"contents\": [{\"id\":1,\"name\":\"cucumbers\",\"quantity\":2},{\"id\":2,\"name\":\"tomatoes\",\"quantity\":2}]}";
     try {
       String response = ClientIO.doPOSTRequest(clientProps.getProperty("endpoint") + request3, data);
       orderNumber = Integer.parseInt(response);
@@ -110,21 +116,50 @@ public class CateringCompanyClientImpTest {
       e.printStackTrace();
     }
 
+    String invalidStatus = String.valueOf(rand.nextInt(10000));
+    String[] statuses = {"packed", "dispatched", "delivered"};
+    int falseOrderNumber = rand.nextInt(10000);
+
     // Null/invalid parameters should be asserted
-    //assertTrue(client.updateOrderStatus(-1, null));
-    //assertTrue(client.updateOrderStatus(orderNumber, null));
-    //assertTrue(client.updateOrderStatus(-1, statuses[0]));
+    //client.updateOrderStatus(-1, null);
+    //client.updateOrderStatus(orderNumber, null);
+    //client.updateOrderStatus(-1, statuses[0]);
 
     // Updating an order with invalid order status
     assertFalse(client.updateOrderStatus(orderNumber, invalidStatus));
-    // Correctly update order status
-    assertTrue(client.updateOrderStatus(orderNumber, statuses[0]));
-    assertTrue(client.updateOrderStatus(orderNumber, statuses[1]));
-    assertTrue(client.updateOrderStatus(orderNumber, statuses[2]));
-    // Updating an already up to date order status
-    assertFalse(client.updateOrderStatus(orderNumber, statuses[2]));
     // Updating an order that has not been placed
     assertFalse(client.updateOrderStatus(falseOrderNumber, statuses[0]));
+
+    // Correctly update order status
+    String request4 = "/requestStatus?order_id=" + orderNumber;
+    try {
+      // Packed
+      assertTrue(client.updateOrderStatus(orderNumber, statuses[0]));
+      String response = ClientIO.doGETRequest(clientProps.getProperty("endpoint") + request4);
+      assertEquals(response, "1");
+
+      // Dispatched
+      assertTrue(client.updateOrderStatus(orderNumber, statuses[1]));
+      response = ClientIO.doGETRequest(clientProps.getProperty("endpoint") + request4);
+      assertEquals(response, "2");
+
+      // Delivered
+      assertTrue(client.updateOrderStatus(orderNumber, statuses[2]));
+      response = ClientIO.doGETRequest(clientProps.getProperty("endpoint") + request4);
+      assertEquals(response, "3");
+
+      // Updating to an already up to date order status
+      assertFalse(client.updateOrderStatus(orderNumber, statuses[2]));
+      response = ClientIO.doGETRequest(clientProps.getProperty("endpoint") + request4);
+      assertEquals(response, "3");
+
+      // Updating to a previous update status
+      assertFalse(client.updateOrderStatus(orderNumber, statuses[0]));
+      response = ClientIO.doGETRequest(clientProps.getProperty("endpoint") + request4);
+      assertEquals(response, "3");
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 
 }
